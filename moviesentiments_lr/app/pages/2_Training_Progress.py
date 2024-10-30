9a3814ce3f6ab7b3c1bf1375cad604d38993d036
anj0la <anjola.aina@gmail.com>
2024-10-30 13:14:20 -0600

    Finished display loss and accuracy graphs generated during training.

--- moviesentiments_lr/app/pages/2_Training_Progress.py
+++ moviesentiments_lr/app/pages/2_Training_Progress.py
@@ -1,7 +1,56 @@
 import pandas as pd
 import streamlit as st
-# from moviesentiments_lr.model.predict import make_prediction
 
+def create_loss_columns(epoch1: int, epoch2: int, lr1: float = 0.1, lr2: float = 0.01):
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.image(f'moviesentiments_lr/figures/loss_epoch_{epoch1}_lr_{lr1}.png')
+
+    with col2:
+        st.image(f'moviesentiments_lr/figures/loss_epoch_{epoch1}_lr_{lr2}.png')
+        
+    with col3:
+        st.image(f'moviesentiments_lr/figures/loss_epoch_{epoch2}_lr_{lr1}.png')
+
+    with col4:
+        st.image(f'moviesentiments_lr/figures/loss_epoch_{epoch2}_lr_{lr2}.png')
+        
+    st.html(f"<p style='text-align: center; font-style: italic;'>Epochs {epoch1} & {epoch2} - Learning Rates: {lr1} & {lr2}</p>")
+    
+def create_accuracy_columns(epoch1: int, epoch2: int, lr1: float = 0.1, lr2: float = 0.01):
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.image(f'moviesentiments_lr/figures/accuracy_epoch_{epoch1}_lr_{lr1}.png')
+
+    with col2:
+        st.image(f'moviesentiments_lr/figures/accuracy_epoch_{epoch1}_lr_{lr2}.png')
+        
+    with col3:
+        st.image(f'moviesentiments_lr/figures/accuracy_epoch_{epoch2}_lr_{lr1}.png')
+
+    with col4:
+        st.image(f'moviesentiments_lr/figures/accuracy_epoch_{epoch2}_lr_{lr2}.png')
+        
+    st.html(f"<p style='text-align: center; font-style: italic;'>Epochs {epoch1} & {epoch2} - Learning Rates: {lr1} & {lr2}</p>")
+
+def display_graphs():
+    st.header('Loss Graphs by Epoch and Learning Rate')
+    create_loss_columns(10, 25)
+
+    # Space between rows for visual clarity
+    st.write(' ') 
+    create_loss_columns(50, 100)
+
+    st.header('Accuracy Graphs by Epoch')
+    create_accuracy_columns(10, 25)
+
+    # Space between rows for visual clarity
+    st.write(' ') 
+    create_accuracy_columns(50, 100)
+
+    
 def run_page():
     # Initalize the page and session variables
     st.set_page_config(page_title='Training Progress', layout='wide')
@@ -9,9 +58,7 @@ def run_page():
     # Set page title and information
     st.title('Training Progress')
     st.write('Listed below are the loss and accuracy graphs generated training, to see the progress of the model. The chosen learning rates were **0.1** and **0.01** as the model learned on these rates. Decreasing the learning rate resulted in a lack of training, hence the more aggresive learning rates.')
-   
-    st.header('Losses over Epochs')
-    
-    st.header('Accuracy over Epochs')
+       
+    display_graphs()
 
 run_page()
