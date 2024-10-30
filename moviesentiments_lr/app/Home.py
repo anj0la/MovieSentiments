8a496aadee3b38c92bddf0a401b05ec83dc40fd2
anj0la <anjola.aina@gmail.com>
2024-10-30 13:27:14 -0600

    Finished adding testing metrics to web app.

--- moviesentiments_lr/app/Home.py
+++ moviesentiments_lr/app/Home.py
@@ -10,16 +10,14 @@ def run_app():
     st.title('Background')    
     st.markdown("""
         
-        This project is designed to analyze IMDB movie reviews using a Logisitic Regression model built from scratch.
-        It was created with the aim of further understanding the basics of a simple neural network, including loss computation, backward propagation, and updating weights.
-        
-        The chosen learning rate was found by experimenting with rates such as 0.1, 0.01 and 0.001. L2 regularization was added to penalize the weights. Early stopping was added to stop the model if there was no improvement after a certain number of epochs.
-        
-        Feel free to explore the following tabs to interact with the model, get some insights into the training process, or view how the accuracy, precision, recall and f1 scores changes as number of epochs increases.
-        
+    This project analyzes IMDB movie reviews using a custom-built Logistic Regression model. It was designed to deepen understanding of foundational neural network concepts by covering key aspects such as loss computation, backpropagation, and weight updates.
+
+    To optimize performance, learning rates of 0.1, 0.01, and 0.001 were tested, and L2 regularization was applied to penalize large weights. Additionally, early stopping was implemented to prevent overfitting by halting training when progress plateaued.
+
+    Explore the tabs to interact with the model and dive deeper into the project details!
+    
     """)
     
-    # st.header('Final Results')
     st.write('*Final model results after training with a learning rate of **0.1** and L2 regularization parameter of **0.001** over **200** epochs.*')
     
     # Define metrics
@@ -29,8 +27,6 @@ def run_app():
     st.markdown(create_cards(test_values), unsafe_allow_html=True)
     
     st.divider()
-
-    
     st.link_button(label='Source code', url='https://github.com/anj0la/MovieSentiments-LR', help='Click to visit source code on GitHub.', type='primary')
 
 run_app()
